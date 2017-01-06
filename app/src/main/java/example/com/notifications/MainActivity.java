0304538ce68e952b0332806e2ff031dbc84b5353
AdamK <Adamkubarek@gmail.com>
2017-01-07 00:41:31 +0100

    everything done

--- app/src/main/java/example/com/notifications/MainActivity.java
+++ app/src/main/java/example/com/notifications/MainActivity.java
@@ -1,10 +1,18 @@
 package example.com.notifications;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +21,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification();
+            }
+        });
+
     }
 
-    
+    private void showNotification () {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentText("This is new notification");
+        builder.setContentTitle("New Notification");
+
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify("Notify", 1, builder.build());
+    }
 }
